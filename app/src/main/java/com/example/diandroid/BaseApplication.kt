b32dd0958efca39be6a43243dc8e5981821d11cc
saurabh <gupta.saurabh@naukri.com>
2022-10-23 11:48:22 +0530

    Added Plugins and DI Example

---
+++ app/src/main/java/com/example/diandroid/BaseApplication.kt
@@ -0,0 +1,7 @@
+package com.example.diandroid
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application()
--- app/src/main/java/com/example/diandroid/MainActivity.kt
+++ app/src/main/java/com/example/diandroid/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.diandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.diandroid.example.Car
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var car : Car
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        car.start()
     }
 }
